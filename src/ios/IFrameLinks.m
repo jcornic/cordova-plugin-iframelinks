cd5b4bb6263f50c3204effc8cc9ef879d22e2ae7
jcornic <jcornic@gmail.com>
2015-10-25 08:37:09 +0100

    Update IFrameLinks.m
    fixed name

--- src/ios/IFrameLinks.m
+++ src/ios/IFrameLinks.m
@@ -5,17 +5,19 @@
 - (BOOL)shouldOverrideLoadWithRequest:(NSURLRequest*)request navigationType:(UIWebViewNavigationType)navigationType
 {
     NSURL *url = [request URL];
-    NSDictionary *settings = [(CDVViewController *)self.viewController settings];
-    
-    NSLog(@"IFrameLinks URL: %@", url);
+    //NSLog(@"IFrameLinks URL: %@", url);
 
-    NSPredicate *hostTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", [settings objectForKey:@"launchexternalforhost"]];
-
-    if ([[url scheme] isEqualToString:@"maps"] || [hostTest evaluateWithObject: [url host]]) {
-        NSLog(@"IFrameLinks Test OK: %@", url);
+    //if ([[url scheme] isEqualToString:@"maps"] || [hostTest evaluateWithObject: [url host]]) {
+    if((navigationType == UIWebViewNavigationTypeLinkClicked) && ([[NSString stringWithFormat:@"%@",url] rangeOfString:@"ligatus"].location != NSNotFound)) {
         if ([[UIApplication sharedApplication] canOpenURL:url]) {
             [[UIApplication sharedApplication] openURL:url];
-            NSLog(@"IFrameLinks openURL: %@", url);
+            return YES;
+        }
+    }
+    
+    if((navigationType == UIWebViewNavigationTypeLinkClicked) && ([[NSString stringWithFormat:@"%@",url] rangeOfString:@"taboola"].location != NSNotFound)) {
+        if ([[UIApplication sharedApplication] canOpenURL:url]) {
+            [[UIApplication sharedApplication] openURL:url];
             return YES;
         }
     }
