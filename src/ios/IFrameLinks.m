aa66051cd1ae6a6b1add04cd1d3ee499c06f2f77
jcornic <jcornic@gmail.com>
2015-10-25 07:27:26 +0100

    Update IFrameLinks.m
    add log

--- src/ios/IFrameLinks.m
+++ src/ios/IFrameLinks.m
@@ -6,12 +6,16 @@
 {
     NSURL *url = [request URL];
     NSDictionary *settings = [(CDVViewController *)self.viewController settings];
+    
+    NSLog(@"IFrameLinks URL: %@", url);
 
     NSPredicate *hostTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", [settings objectForKey:@"launchexternalforhost"]];
 
     if ([[url scheme] isEqualToString:@"maps"] || [hostTest evaluateWithObject: [url host]]) {
+        NSLog(@"IFrameLinks Test OK: %@", url);
         if ([[UIApplication sharedApplication] canOpenURL:url]) {
             [[UIApplication sharedApplication] openURL:url];
+            NSLog(@"IFrameLinks openURL: %@", url);
             return YES;
         }
     }
